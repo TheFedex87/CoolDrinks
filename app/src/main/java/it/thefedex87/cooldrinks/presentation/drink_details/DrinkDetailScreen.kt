1052c024b16d36d57f58a0d5a0fed46c1754a703
Federico Creti <federico.creti@solaredge.com>
2022-07-20 15:48:28 +0200

    Changed UI of DrinkDetailScreen.kt

--- app/src/main/java/it/thefedex87/cooldrinks/presentation/drink_details/DrinkDetailScreen.kt
+++ app/src/main/java/it/thefedex87/cooldrinks/presentation/drink_details/DrinkDetailScreen.kt
@@ -1,10 +1,14 @@
 package it.thefedex87.cooldrinks.presentation.drink_details
 
 import android.util.Log
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -15,6 +19,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
@@ -27,9 +32,10 @@ import it.thefedex87.cooldrinks.presentation.drink_details.components.Characteri
 import it.thefedex87.cooldrinks.presentation.drink_details.components.IngredientItem
 import it.thefedex87.cooldrinks.presentation.ui.bottomnavigationscreen.BottomNavigationScreenState
 import it.thefedex87.cooldrinks.presentation.ui.theme.LocalSpacing
-import it.thefedex87.cooldrinks.presentation.ui.theme.Red40
 import it.thefedex87.cooldrinks.presentation.util.calcDominantColor
 import it.thefedex87.cooldrinks.util.Consts
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -127,7 +133,7 @@ fun DrinkDetailScreen(
                 brush = Brush.verticalGradient(
                     listOf(
                         Color.Transparent,
-                        dominantColor
+                        dominantColor!!
                     )
                 )
             )
@@ -140,137 +146,237 @@ fun DrinkDetailScreen(
         }
     } else {
         onDrinkLoaded?.invoke(viewModel.state.drinkName!!)
-        Column(
+        /* Column(
+             modifier = Modifier
+                 .fillMaxSize()
+                 .nestedScroll(appBarScrollBehavior.nestedScrollConnection)
+                 .padding(horizontal = spacing.spaceSmall)
+                 .verticalScroll(rememberScrollState())
+         ) {*/
+        val closeCardHeight = 62.dp
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .nestedScroll(appBarScrollBehavior.nestedScrollConnection)
-                .padding(horizontal = spacing.spaceSmall)
-                .verticalScroll(rememberScrollState())
         ) {
             Box(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxSize()
+                    .padding(top = 80.dp, bottom = 8.dp, start = 8.dp, end = 8.dp)
+                    .align(Alignment.TopStart)
             ) {
+                var expandedIndex by remember {
+                    mutableStateOf(0)
+                }
+
                 ElevatedCard(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .align(alignment = Alignment.TopCenter)
-                        .padding(
-                            top = 80.dp,
-                            start = spacing.spaceSmall,
-                            end = spacing.spaceSmall,
-                            bottom = spacing.spaceSmall
-                        ),
-                    elevation = CardDefaults.elevatedCardElevation(
-                        defaultElevation = 4.dp
-                    )
+                        .fillMaxSize(),
+                    onClick = {
+                        expandedIndex = 0
+                    },
+                    shape = RoundedCornerShape(31.dp)
                 ) {
-                    Column(
+                    BoxWithConstraints(
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxSize()
+                            .animateContentSize()
+                            .padding(top = 180.dp)
                     ) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(
-                                    top = 180.dp,
-                                    start = spacing.spaceSmall,
-                                    end = spacing.spaceSmall,
-                                    bottom = spacing.spaceSmall
-                                )
-                        ) {
-                            CharacteristicSection(
-                                drinkGlass = viewModel.state.drinkGlass,
-                                drinkCategory = viewModel.state.drinkCategory,
-                                drinkAlcoholic = viewModel.state.drinkAlcoholic
-                            )
+                        val maxHeight = this.maxHeight
+                        val expandedHeight = maxHeight - closeCardHeight * 3
+
+                        val anim1 = remember {
+                            androidx.compose.animation.core.Animatable(1f)
+                        }
+                        val anim2 = remember {
+                            androidx.compose.animation.core.Animatable(0f)
+                        }
+                        val anim3 = remember {
+                            androidx.compose.animation.core.Animatable(0f)
                         }
 
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(
-                                    start = spacing.spaceSmall,
-                                    end = spacing.spaceSmall,
-                                    bottom = spacing.spaceSmall
-                                ),
-                        ) {
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(spacing.spaceSmall),
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                Text(
-                                    text = "Ingredients",
-                                    style = MaterialTheme.typography.headlineSmall
-                                )
-                                Spacer(modifier = Modifier.height(spacing.spaceSmall))
-                                viewModel.state.drinkIngredients?.map {
-                                    IngredientItem(
-                                        ingredientName = it.first,
-                                        ingredientMeasure = it.second
+                        LaunchedEffect(key1 = expandedIndex) {
+                            when(expandedIndex) {
+                                0 -> {
+                                    awaitAll(
+                                        async { anim1.animateTo(1f) },
+                                        async { anim2.animateTo(0f) },
+                                        async { anim3.animateTo(0f) }
+                                    )
+                                }
+                                1 -> {
+                                    awaitAll(
+                                        async { anim1.animateTo(0f) },
+                                        async { anim2.animateTo(1f) },
+                                        async { anim3.animateTo(0f) }
+                                    )
+                                }
+                                else -> {
+                                    awaitAll(
+                                        async { anim1.animateTo(0f) },
+                                        async { anim2.animateTo(0f) },
+                                        async { anim3.animateTo(1f) }
                                     )
                                 }
                             }
                         }
-                        viewModel.state.drinkInstructions?.let { instructions ->
-                            Text(
-                                modifier = Modifier
-                                    .padding(spacing.spaceMedium),
-                                text = instructions
-                            )
-                        }
-                    }
-                }
-                Box(
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopCenter)
-                        .size(245.dp)
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.background),
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .align(alignment = Alignment.Center)
-                            .size(235.dp)
-                            .clip(CircleShape)
-                            .background(
-                                (dominantColor
-                                    ?: MaterialTheme.colorScheme.background).copy(alpha = 0.6f)
-                            ),
-                    ) {
-                        AsyncImage(
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data(viewModel.state.drinkImagePath)
-                                .crossfade(true)
-                                .build(),
+
+                        Column(
                             modifier = Modifier
-                                .clip(CircleShape)
-                                .size(220.dp)
-                                .align(Alignment.Center),
-                            onLoading = {
-                                R.drawable.search_background
-                            },
-                            onSuccess = {
-                                if (calculatedDominantColor == null) {
-                                    Log.d(Consts.TAG, "Calculate dominant color")
-                                    calcDominantColor(it.result.drawable, null) { color ->
-                                        dominantColor = color
+                                .fillMaxSize()
+                        ) {
+                            Text(
+                                text = "About",
+                                style = MaterialTheme.typography.headlineSmall,
+                                modifier = Modifier
+                                    .padding(vertical = 8.dp, horizontal = 16.dp)
+                                    .height(closeCardHeight - 16.dp)
+                            )
+                            CharacteristicSection(
+                                drinkGlass = viewModel.state.drinkGlass,
+                                drinkCategory = viewModel.state.drinkCategory,
+                                drinkAlcoholic = viewModel.state.drinkAlcoholic,
+                                modifier = Modifier
+                                    .height(
+                                        expandedHeight * anim1.value
+                                    )
+                            )
+                            ElevatedCard(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                onClick = {
+                                    expandedIndex = 1
+                                },
+                                colors = CardDefaults.elevatedCardColors(
+                                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                                ),
+                                shape = RoundedCornerShape(31.dp)
+                            ) {
+                                Column(
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        text = "Ingredients",
+                                        style = MaterialTheme.typography.headlineSmall,
+                                        modifier = Modifier
+                                            .padding(vertical = 8.dp, horizontal = 16.dp)
+                                            .height(closeCardHeight - 16.dp)
+                                    )
+                                    //Spacer(modifier = Modifier.height(spacing.spaceSmall))
+                                    Box(modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 8.dp)
+                                        .height(
+                                            expandedHeight * anim2.value
+                                        )) {
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .verticalScroll(rememberScrollState())
+                                        ) {
+                                            viewModel.state.drinkIngredients?.map {
+                                                IngredientItem(
+                                                    ingredientName = it.first,
+                                                    ingredientMeasure = it.second
+                                                )
+                                            }
+                                        }
+                                    }
+
+                                    ElevatedCard(
+                                        modifier = Modifier
+                                            .fillMaxWidth(),
+                                        onClick = {
+                                            expandedIndex = 2
+                                        },
+                                        colors = CardDefaults.elevatedCardColors(
+                                            containerColor = MaterialTheme.colorScheme.surfaceVariant
+                                        ),
+                                        shape = RoundedCornerShape(31.dp)
+                                    ) {
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                        ) {
+                                            Text(
+                                                text = "Instructions",
+                                                style = MaterialTheme.typography.headlineSmall,
+                                                modifier = Modifier
+                                                    .padding(vertical = 8.dp, horizontal = 16.dp)
+                                                    .height(closeCardHeight - 16.dp)
+                                            )
+                                            Box(modifier = Modifier
+                                                .padding(8.dp)
+                                                .height(expandedHeight * anim3.value)
+                                            ) {
+                                                viewModel.state.drinkInstructions?.let {
+                                                    Text(
+                                                        modifier = Modifier
+                                                            .height(
+                                                                expandedHeight * anim3.value
+                                                            )
+                                                            .verticalScroll(state = rememberScrollState()),
+                                                        text = it
+                                                    )
+                                                }
+                                            }
+                                        }
                                     }
                                 }
-                            },
-                            onError = {
-                                R.drawable.search_background
-                            },
-                            contentDescription = viewModel.state.drinkName
-                        )
+                            }
+                        }
                     }
+
                 }
             }
-            /*if (drinkId == null) {
-                Spacer(modifier = Modifier.height(70.dp))
-            }*/
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopCenter)
+                    .size(245.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.background),
+            ) {
+                Box(
+                    modifier = Modifier
+                        .align(alignment = Alignment.Center)
+                        .size(235.dp)
+                        .clip(CircleShape)
+                        .background(
+                            (dominantColor
+                                ?: MaterialTheme.colorScheme.background).copy(alpha = 0.6f)
+                        ),
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(viewModel.state.drinkImagePath)
+                            .crossfade(true)
+                            .build(),
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(220.dp)
+                            .align(Alignment.Center),
+                        onLoading = {
+                            R.drawable.search_background
+                        },
+                        onSuccess = {
+                            if (calculatedDominantColor == null) {
+                                Log.d(Consts.TAG, "Calculate dominant color")
+                                calcDominantColor(it.result.drawable, null) { color ->
+                                    dominantColor = color
+                                }
+                            }
+                        },
+                        onError = {
+                            R.drawable.search_background
+                        },
+                        contentDescription = viewModel.state.drinkName
+                    )
+                }
+            }
         }
+        /*if (drinkId == null) {
+            Spacer(modifier = Modifier.height(70.dp))
+        }*/
+        //}
     }
 }
