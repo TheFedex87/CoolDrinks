f12d5479480160f47a9e7e8a59176a32f5f8e3a4
Federico Creti <federico.creti87@gmail.com>
2023-08-02 12:30:20 +0200

    Added satus bar color based on color scheme

--- app/src/main/java/it/thefedex87/cooldrinks/presentation/ui/theme/Theme.kt
+++ app/src/main/java/it/thefedex87/cooldrinks/presentation/ui/theme/Theme.kt
@@ -1,11 +1,16 @@
 package it.thefedex87.cooldrinks.presentation.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColorScheme(
     primary = Violet80,
@@ -76,6 +81,18 @@ fun CoolDrinksTheme(
         useDynamicColors && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
         darkTheme -> DarkColorPalette
         else -> LightColorPalette
+    }
+
+    val view = LocalView.current
+    if(!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+            WindowCompat.getInsetsController(
+                window,
+                view
+            ).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
